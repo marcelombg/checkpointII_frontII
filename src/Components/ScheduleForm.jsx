d4c7be83d3b952ef8a7f464d63504466e35e6d2c
marcelo.muniz <marcelo_mbg@hotmail.com>
2022-12-13 11:10:26 -0300

    POST montado para ScheduleForm

--- src/Components/ScheduleForm.jsx
+++ src/Components/ScheduleForm.jsx
@@ -1,23 +1,17 @@
-import { useEffect, useInsertionEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ScheduleForm.module.css";
 import React from "react";
 import axios from "axios";
-import { response } from "msw";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 const ScheduleForm = () => {
 
   const [dentist, setDentist] = useState([])
   const [pacient, setPacient] = useState([])
   const [date, setDate] = useState([])
-  const [formularioErro, setFormularioErro] = useState(false)
-  const [authToken, setAuthToken] = useState('')
-
-  const [post, setPost] = useState()
-  const api = axios.create({
-    baseURL: "http://dhodonto.ctdprojetos.com.br",
-  });
-
-
+  const [token, setToken] = useState('');
+  
   useEffect(() => {
     //Nesse useEffect, você vai fazer um fetch na api buscando TODOS os dentistas
     //e pacientes e carregar os dados em 2 estados diferentes
@@ -32,7 +26,7 @@ const ScheduleForm = () => {
         )
       }
     )
-    
+
     fetch('https://dhodonto.ctdprojetos.com.br/paciente').then(
       response => {
         response.json().then(
@@ -41,7 +35,7 @@ const ScheduleForm = () => {
           }
         )
       }
-    )    
+    )
 
   }, []);
 
@@ -54,130 +48,170 @@ const ScheduleForm = () => {
     //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
 
     event.preventDefault()
-  
-    if (dentist === '' || pacient === '' || date === '') {
 
-      setFormularioErro(true)
+    setToken(localStorage.getItem('token'));
+
+    if (dentist === '' || pacient === '' || date === '' || token === null || token === ''){
+
+      toast.error('Um ou mais campos não preenchidos.')
 
     } else {
-      setFormularioErro(false)
+
+      console.log(token)
+
+      const requestConfig = {
+        method: 'POST',
+        headers: {
+          'Authentication': 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+          {
+            "paciente":
+              {
+                "nome": "teste",
+                "sobrenome": "teste",
+                "matricula": "5dfce4f7-3d56-47d3-a442-b52c21c5d1f8",
+                "usuario": {
+                  "username": "testeteste"
+                },
+                "endereco": {
+                  "id": 1,
+                  "logradouro": "Rua das aboboras",
+                  "numero": "175",
+                  "complemento": "N/A",
+                  "bairro": "Centro",
+                  "municipio": "Osasco",
+                  "estado": "SP",
+                  "cep": "1877955",
+                  "pais": "BR"
+                },
+                "dataDeCadastro": "2022-11-08T14:31:58.813+00:00"
+              },
+            "dentista":
+              {
+                "nome": "Admin",
+                "sobrenome": "Admin",
+                "matricula": "c3e6cf30-dccc-4e21-935a-8efe9344677e",
+                "usuario": {
+                  "username": "dentistaAdmin"
+                }
+              },
+            "dataHoraAgendamento": "2022-12-30T11:04"
+          }
+        )
+      }
+
+      console.log(requestConfig)
+  
+      fetch('https://dhodonto.ctdprojetos.com.br/consulta', requestConfig)
+      .then(
+        response => {
+          response.json().then(
+            data => {console.log(data)
+
+            toast.success('Consulta marcada com sucesso!');
+            })
+            .catch(e => {
+            toast.error('Error ao enviar a requisição.')
+            })
+          }
+      )
 
       setDate('')
 
-      alert('Consulta marcada com sucesso!')
+    }
 
-      if(authToken !== '') {
-      
-        api
-        .post("/consulta",
-        {
-          pacient,
-          dentist,
-          date
-        })
-        .then((response) => setPost(response.data))
-        .catch((err) => {
-          console.error("ops! ocorreu um erro" + err);
-        });
+  }
+
+return (
+  <>
+   <ToastContainer />
+    {/* //Na linha seguinte deverá ser feito um teste se a aplicação
+        // está em dark mode e deverá utilizar o css correto */}
+    <div
+      className={`text-center container}`
       }
-    } 
-    
-  };
+    >
+      <form onSubmit={handleSubmit}>
+        <div className={`row ${styles.rowSpacing}`}>
+          <div className="col-sm-12 col-lg-6">
+            <label htmlFor="dentist" className="form-label">
+              Dentist
+            </label>
+            <select className="form-select" name="dentist" id="dentist">
 
-  return (
-    <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar o css correto */}
-      <div
-        className={`text-center container}`
-        }
-      >
-        <form onSubmit={handleSubmit} className={formularioErro ? 'form-error' : ''}>
-          <div className={`row ${styles.rowSpacing}`}>
-            <div className="col-sm-12 col-lg-6">
-              <label htmlFor="dentist" className="form-label">
-                Dentist
-              </label>
-              <select className="form-select" name="dentist" id="dentist">
-
-                {
-                  dentist.map(dentistlist => {
-                      return (
-                        <option
-                          key={dentistlist.matricula}
-                          value={dentistlist.matricula}
-                        >
-                          {dentistlist.nome} {dentistlist.sobrenome}
-                        </option>
-                      )
-                    }
+              {
+                dentist.map(dentistlist => {
+                  return (
+                    <option
+                      key={dentistlist.matricula}
+                      value={dentistlist.matricula}
+                    >
+                      {dentistlist.nome} {dentistlist.sobrenome}
+                    </option>
                   )
                 }
+                )
+              }
 
-              </select>
-            </div>
+            </select>
+          </div>
 
-            <div className="col-sm-12 col-lg-6">
-              <label htmlFor="patient" className="form-label">
-                Patient
-              </label>
-              <select className="form-select" name="patient" id="patient">
+          <div className="col-sm-12 col-lg-6">
+            <label htmlFor="patient" className="form-label">
+              Patient
+            </label>
+            <select className="form-select" name="patient" id="patient">
 
-                {
-                    pacient.map(pacienteList => {
-                      return (
-                        <option
-                          key={pacienteList.matricula}
-                          value={pacienteList.matricula}
-                        >
-                          {pacienteList.nome} {pacienteList.sobrenome}
-                        </option>
-                      )
-                    }
+              {
+                pacient.map(pacienteList => {
+                  return (
+                    <option
+                      key={pacienteList.matricula}
+                      value={pacienteList.matricula}
+                    >
+                      {pacienteList.nome} {pacienteList.sobrenome}
+                    </option>
                   )
                 }
+                )
+              }
 
-              </select>
-            </div>
-
+            </select>
           </div>
 
-          <div className={`row ${styles.rowSpacing}`}>
-            <div className="col-12">
-              <label htmlFor="appointmentDate" className="form-label">
-                Date
-              </label>
-              <input
-                className="form-control"
-                id="appointmentDate"
-                name="appointmentDate"
-                type="datetime-local"
-                value={date}
-                onChange={event => setDate(event.target.value)}
-              />
-            </div>
+        </div>
+
+        <div className={`row ${styles.rowSpacing}`}>
+          <div className="col-12">
+            <label htmlFor="appointmentDate" className="form-label">
+              Date
+            </label>
+            <input
+              className="form-control"
+              id="appointmentDate"
+              name="appointmentDate"
+              type="datetime-local"
+              value={date}
+              onChange={event => setDate(event.target.value)}
+            />
           </div>
-          <div className={`row ${styles.rowSpacing}`}>
-            {/* //Na linha seguinte deverá ser feito um teste se a aplicação
+        </div>
+        <div className={`row ${styles.rowSpacing}`}>
+          {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
-            <button
-              className={`btn btn-light ${styles.button}`}
-              type="submit"
-              onSubmit={event => handleSubmit(event)}
-            >
-              Schedule
-            </button>
-
-            {
-                formularioErro ? (
-                    <span>Um ou mais campos não preenchidos</span>
-                ) : null
-            }
-
-          </div>
-        </form>
-      </div>
-    </>
+          <button
+            className={`btn btn-light ${styles.button}`}
+            type="submit"
+            onSubmit={event => handleSubmit(event)}
+          >
+            Schedule
+          </button>
+        </div>
+      </form>
+    </div>
+  </>
   );
 };
 
